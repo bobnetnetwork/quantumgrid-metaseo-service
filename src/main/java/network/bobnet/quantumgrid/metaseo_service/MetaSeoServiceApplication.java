d8ec44c0b7d9f7dad44365062fd4b26d0cbd8674
smaso_JaszayN <bob@bobnet.network>
2024-09-02 22:12:35 +0200

    QUAN-56 Set up project structure using Spring Boot

---
+++ src/main/java/network/bobnet/quantumgrid/metaseo_service/MetaSeoServiceApplication.java
@@ -0,0 +1,16 @@
+package network.bobnet.quantumgrid.metaseo_service;
+
+import lombok.Generated;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Generated
+@SpringBootApplication
+public class MetaSeoServiceApplication {
+
+	@Generated
+	public static void main(String[] args) {
+		SpringApplication.run(MetaSeoServiceApplication.class, args);
+	}
+
+}
